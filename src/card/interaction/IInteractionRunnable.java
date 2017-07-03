ba91a3120140de82a9e008b84eacbf590cd4beaf
Lucas PRANEUF <lucas.praneuf@gmail.com>
2017-07-03 03:24:16 +0200

    Java 8 compliant : change abstract class to interface

--- src/card/interaction/IInteractionRunnable.java
+++ src/card/interaction/IInteractionRunnable.java
@@ -1,7 +1,19 @@
 package card.interaction;
 
-interface IInteractionRunnable
+import java.util.List;
+
+public interface IInteractionRunnable
 {
+	public default void run(List<IInteractionContext> circumstances,
+			List<IInteractionTarget> targets) throws PropertyRunnableException
+	{
+		if (!isAllowToRun(circumstances.toArray(new IInteractionContext[circumstances.size()])))
+		{
+			throw new PropertyRunnableException(circumstances, targets);
+		}
+		runInternal(targets.toArray(new IInteractionTarget[targets.size()]));
+	}
+
 	boolean isAllowToRun(IInteractionContext... circumstances);
 
 	void runInternal(IInteractionTarget... targets);
