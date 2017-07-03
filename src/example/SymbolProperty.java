046c8f445462f9d3fe1e09b3be6506439402acf4
Lucas PRANEUF <lucas.praneuf@gmail.com>
2017-07-03 19:46:07 +0200

    Add Serial Version UID

--- src/example/SymbolProperty.java
+++ src/example/SymbolProperty.java
@@ -4,6 +4,8 @@ import card.property.CardPropertyWithValue;
 
 public class SymbolProperty extends CardPropertyWithValue<Symbol>
 {
+	private static final long serialVersionUID = 1L;
+
 	public SymbolProperty(Symbol value)
 	{
 		super("Symbol", "Card symbol", value);
--- src/example/ValueProperty.java
+++ src/example/ValueProperty.java
@@ -4,6 +4,8 @@ import card.property.CardPropertyWithValue;
 
 public class ValueProperty extends CardPropertyWithValue<Value>
 {
+	private static final long serialVersionUID = 1L;
+
 	public ValueProperty(Value value)
 	{
 		super("Value", "Card value", value);
